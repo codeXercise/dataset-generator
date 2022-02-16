993ce9a2c34b0fdde3965d39bc14583fb52a09df
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-02-16 22:43:26 +0100

    added dataset 1 generator

--- app.js
+++ app.js
@@ -22,6 +22,56 @@ rl.question('> ', function(answer) {
 
             case '1':
 
+                // Log
+                console.log("Generating dataset for challenge 1...");
+
+                // Create a new file
+                fs.writeFile('dataset_1.txt', '', function(err) {
+
+                    if (err) {
+                        return console.log(err);
+                    }
+
+                    // Loop through the lines
+                    for (var i = 0; i < answer2; i++) {
+
+                        // Generate a random number between 1 and 9
+                        var rocket = Math.floor(Math.random() * 9) + 1;
+
+                        // Randomly generate a number between 1 and 100
+                        var fuel = Math.floor(Math.random() * 100) + 1;
+
+                        // Add "0"`s to the number so that it is 3 digits long
+                        if (fuel < 10) {
+                            fuel = "00" + fuel;
+                        } else if (fuel < 100) {
+                            fuel = "0" + fuel;
+                        }
+
+                        // Generate a 0 or 1
+                        var success = Math.floor(Math.random() * 2);
+
+                        // Generate the string
+                        var line = rocket + '-' + fuel + '-' + success + '\n';
+
+                        // Write the line to the file
+                        fs.appendFile('dataset_1.txt', line, function(err) {
+
+                            if (err) {
+                                return console.log(err);
+                            }
+
+                        });
+
+                    }
+
+                    // Log
+                    console.log("Dataset generated!");
+                    console.log("Dataset saved to dataset_1.txt");
+
+                });
+
+
                 break;
 
             case '2':
